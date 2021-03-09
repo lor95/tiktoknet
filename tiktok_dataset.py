9a07107955412c9a2be54ac1857fc09b16f60325
silviacecch <80323532+silviacecch@users.noreply.github.com>
2021-03-09 13:22:34 +0100

    Update tiktok_dataset.py

--- tiktok_dataset.py
+++ tiktok_dataset.py
@@ -1,1 +1,7 @@
 from TikTokApi import TikTokApi # api wrapper import https://github.com/davidteather/TikTok-Api
+api = TikTokApi.get_instance(use_test_endpoints=True)
+
+tiktoks=api.byHashtag("ITookANap",count=100000,custom_verifyFp="")
+
+for tiktok in tiktoks:
+    print(tiktok)
