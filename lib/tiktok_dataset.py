f7592b42b73769c57243266995b6d3aac3b69647
Lorenzo Giuliani <lory_extr3me@hotmail.it>
2021-03-20 11:09:26 +0100

    recovery

--- lib/tiktok_dataset.py
+++ lib/tiktok_dataset.py
@@ -82,25 +82,31 @@ def buildDatasetByHashtag(api, hashtag, url_orig, _count=10000):
     df.loc[df['id'] == _id, 'originalVideo'] = 1 # search for the original video and flag it
     df.to_csv("dataset/dataset_"+hashtag+".csv", sep=';', index=False)
 
-def pubAuthList(api, dataset): # returns dataset of users with public liked tiktok's list
+def pubAuthList(api, dataset, recovery): # returns dataset of users with public liked tiktok's list
     logger = logging.getLogger()
-    df = pd.read_csv("./dataset/dataset_"+dataset+".csv", sep=";")
-    counter = 1
-    size = df.shape[0]
-    df1 = pd.DataFrame({'author_id': [], 'author_secUid': [], 'author_nickname': []}, dtype="string")
     logger.disabled = True
+    df = pd.read_csv("./dataset/dataset_"+dataset+".csv", sep=";")
+    counter = 0
+    size = df.shape[0]
+    if recovery == 0:
+        df1 = pd.DataFrame({'author_id': [], 'author_secUid': [], 'author_nickname': []}, dtype="string")
+    else:
+        df1 = pd.read_csv("./dataset/authors/pubLiked_"+dataset+".csv", sep=";")
     try:
         for index, row in df.iterrows():
-            print("Parsing: "+str(counter)+"/"+str(size))
             counter += 1
-            if len(api.userLiked(row['author_id'],row['author_secUid'],count=1)) > 0:
-                tempDict = {'author_id': row['author_id'], 'author_secUid': row['author_secUid'], 'author_nickname': row['author_nickname']}
-                df1 = df1.append(tempDict, ignore_index=True)
-            #if df1.shape[0] == 15: # subset of users
-                #break         
+            if index >= recovery:
+                print("Parsing: "+str(counter)+"/"+str(size))
+                if len(api.userLiked(row['author_id'],row['author_secUid'],count=1)) > 0:
+                    tempDict = {'author_id': row['author_id'], 'author_secUid': row['author_secUid'], 'author_nickname': row['author_nickname']}
+                    df1 = df1.append(tempDict, ignore_index=True)
+                #if df1.shape[0] == 15: # subset of users
+                    #break     
     except:
-        print("Error, something went wrong.")
-        # il programma deve essere riavviato per continuare
+        logger.disabled = False
+        df1.drop_duplicates(keep='first',inplace=True)
+        df1.to_csv("dataset/authors/pubLiked_"+dataset+".csv", sep=";", index=False)
+        raise Exception(counter)    
     logger.disabled = False
     df1.drop_duplicates(keep='first',inplace=True)
     df1.to_csv("dataset/authors/pubLiked_"+dataset+".csv", sep=";", index=False)
--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from TikTokApi import TikTokApi
 import lib.tiktok_dataset as td
+import argparse
+import os
+import sys
+
+print(sys.argv)
+parser = argparse.ArgumentParser(description = 'TikTok dataset generator.')
+parser.add_argument('-rec', action = 'store', type = int,
+                    help = 'Recovery mode: default 0 (False)',
+                    metavar = '<recovery_mode>',
+                    default = 0)
+args = parser.parse_args()
 
 challenges = { "itookanap": 
                 {"name": "ITookANap",
@@ -9,7 +20,15 @@ challenges = { "itookanap":
 
 api = TikTokApi.get_instance(use_test_endpoints=True)
 
-td.buildDatasetByHashtag(api, challenges["itookanap"]["name"], challenges["itookanap"]["url"], 10000)
-td.pubAuthList(api, challenges["itookanap"]["name"])
+sys.argv = sys.argv[:1] 
+
+if args.rec < 0:
+    td.buildDatasetByHashtag(api, challenges["itookanap"]["name"], challenges["itookanap"]["url"], 10000)
+try:
+    td.pubAuthList(api, challenges["itookanap"]["name"], args.rec)
+except Exception as ex:
+    sys.argv.append("-rec")
+    sys.argv.append(str(ex.args[0]))
+    os.execv(sys.executable, [sys.executable] + sys.argv)
 td.checkConnections(api, challenges["itookanap"]["name"])
 td.ETL(challenges["itookanap"]["name"])
