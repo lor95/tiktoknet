6ff6354626fe5f3a113ea54503cfbb792e8aa5d8
silviacecch <80323532+silviacecch@users.noreply.github.com>
2021-03-22 10:22:39 +0100

    Delete challenges.py

--- lib/challenges.py
+++
@@ -1,8 +0,0 @@
-challenges = { "itookanap": 
-                {"name": "ITookANap",
-                "url": "https://www.tiktok.com/@gunnarolla/video/6816020939759815942"} # original video
-                # insert challenge here
-            }
-
-def getChallenge(challenge):
-    return challenges[challenge]
