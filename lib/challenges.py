f9c4192622f187f9e8d76220a2763e56123f25d9
silviacecch <silvia.cecchini@icloud.com>
2021-03-24 09:57:25 +0100

    Update challenges.py
    new challenges

--- lib/challenges.py
+++ lib/challenges.py
@@ -6,10 +6,33 @@ challenges = { "itookanap":
                     "url":""},
                 "ohnanachallenge":
                     {"name":"ohnanachallenge",
-                    "url":"https://www.tiktok.com/@saffronbarker/video/6778137328499084549"}
+                    "url":"https://www.tiktok.com/@saffronbarker/video/6778137328499084549"},
+                "makarenachallenge":
+                    {"name":"makarenachallenge",
+                     "url":""},
+                "weirdsoundchallenge":
+                    {"name":"weirdsoundchallenge",
+                     "url":"https://www.tiktok.com/@angelbrown_19/video/6806305248223759622"},
+                "copinesdancechallenge":
+                    {"name":"copinesdancechallenge",
+                     "url":"https://www.tiktok.com/@nicolesopogee/video/6906089727640816897"},
+                "eatingchalk":
+                    {"name":"eatingchalkasmr",
+                     "url":""},
+                "saltandicechallenge":
+                    {"name":"saltandicechallenge",
+                     "url":""},
+                "trashbagchallenge":
+                    {"name": "trashbagchallenge",
+                     "url":""},
+                "walkamile":
+                    {"name":"walkamile",
+                     "url":""},
+               "silhouettechallenge":
+                    {"name":"silhouettechallenge",
+                     "url":""}
                 # insert challenge here
             }
-
 def getChallenge(challenge):
     return challenges[challenge.lower()]
 
