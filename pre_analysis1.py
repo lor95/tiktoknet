63e5c9f37796499b3143150638f1d21d601d08fd
lor95 <lory_extr3me@hotmail.it>
2021-06-22 15:31:38 +0200

    clust coeff

--- pre_analysis1.py
+++ pre_analysis1.py
@@ -26,35 +26,42 @@ NEG_CHALLENGES = ["silhouettechallenge",
 ALL_CHALLENGES = [POS_CHALLENGES, NEG_CHALLENGES]
 PLOT = [[],[]]
 PLOTDENSITY = [[[] for x in POS_CHALLENGES],[[] for x in NEG_CHALLENGES]]
+PLOTCOEFF = [[[] for x in POS_CHALLENGES],[[] for x in NEG_CHALLENGES]]
 
 def reset_stats():
-    return {"density":[[] for x in range(0,20)]}
+    return {"density":[[] for x in range(0,20)],
+            "coeff": [[] for x in range(0,20)]}
 
 def print_results(arr, _type=False):
-    text = "POSITIVE"
     if _type:
-        text = "NEGATIVE"
         for val in arr["density"]:
             for i in range(len(NEG_CHALLENGES)):
                 PLOTDENSITY[1][i].append(val[i])
+        for val in arr["coeff"]:
+            for i in range(len(NEG_CHALLENGES)):
+                PLOTCOEFF[1][i].append(val[i])
     else:
         for val in arr["density"]:
             for i in range(len(POS_CHALLENGES)):
                 PLOTDENSITY[0][i].append(val[i])
+        for val in arr["coeff"]:
+            for i in range(len(POS_CHALLENGES)):
+                PLOTCOEFF[0][i].append(val[i])
     
 flag = False
-counter = 0
 for elem in ALL_CHALLENGES:
     STATS = reset_stats()
     for challenge in elem:
         for i in range(0,100,5):
-            graph, _, _, _, _, _, _, _ = ntx.graphCalculation(challenges.getChallenge(challenge)["name"].split(",")[0], intervals = [i,i+5])
+            graph, _, _, _, _, _, _, _ = ntx.graphCalculation(challenges.getChallenge(challenge)["name"].split(",")[0], intervals = [0,i+5])
             graph = graph.to_undirected()
             STATS['density'][int(round(i/5))].append(nx.density(graph))
-        counter += 1
+            if(graph.number_of_nodes() == 0):
+                STATS['coeff'][int(round(i/5))].append(0)
+            else:
+                STATS['coeff'][int(round(i/5))].append(nx.average_clustering(graph))
     print_results(STATS, flag)
     flag = True
-    counter = 0
 
 #plotpos
 for i in range(len(POS_CHALLENGES)):
@@ -64,7 +71,7 @@ for i in range(len(POS_CHALLENGES)):
     plt.xticks(range(0,101,5))
     plt.grid("--")
     plt.gca().set_xlim(xmin=0, xmax=100)
-    plt.plot(range(0,101,5), [0]+PLOTDENSITY[0][i], label=POS_CHALLENGES[i])
+    plt.plot(range(0,101,5), [0]+PLOTCOEFF[0][i], label=POS_CHALLENGES[i])
     #    if point != 100:
     #        plt.axvline(x=point,color='r', linewidth=2)
     plt.show()
@@ -76,7 +83,7 @@ for i in range(len(NEG_CHALLENGES)):
     plt.xticks(range(0,101,5))
     plt.grid("--")
     plt.gca().set_xlim(xmin=0, xmax=100)
-    plt.plot(range(0,101,5), [0]+PLOTDENSITY[1][i], label=NEG_CHALLENGES[i])
+    plt.plot(range(0,101,5), [0]+PLOTCOEFF[1][i], label=NEG_CHALLENGES[i])
     #    if point != 100:
     #        plt.axvline(x=point,color='r', linewidth=2)
     plt.show()
