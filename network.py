577f0081bc3a82ad37d03bb9d5427976cad0f0af
lor95 <lory_extr3me@hotmail.it>
2021-06-23 16:29:01 +0200

    gephi

--- network.py
+++ network.py
@@ -2,8 +2,11 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import lib.tiktok_network as ntx
 
-plt.figure(figsize=(15,15))
-graph, labels, colors, pos, _, _, _, _ = ntx.graphCalculation("bussitchallenge",colorCriteria="createTime")
+#plt.figure(figsize=(15,15))
+for i in range(0,100,10):
+    graph, labels, colors, pos, _, _, _, _ = ntx.graphCalculation("bussitchallenge",colorCriteria="createTime", intervals=[i,i+10])
+    nx.write_gexf(graph, "dataset/bussitchallenge"+str(int(i+10))+".gexf", prettyprint=True)
+'''
 ntx.graphStats(graph)
 nx.draw_networkx_nodes(graph,pos,node_color=colors,node_size=60)     
 nx.draw_networkx_labels(graph, pos, labels,font_size=5)
@@ -11,3 +14,4 @@ nx.draw_networkx_edges(graph,pos,arrows=True,arrowsize=3,arrowstyle="-|>",alpha=
 ax= plt.gca()
 ax.collections[0].set_edgecolor("#000000")
 plt.show()
+'''
